2c972e53949828943a8f2114b5335598fa6a9a06
spendar <372782125@qq.com>
2021-02-28 17:25:11 +0800

    完成了Django的入门部分，构建了一个简单的投票系统。

--- polls/models.py
+++ polls/models.py
@@ -10,11 +10,17 @@ class Question(models.Model):
     pub_date = models.DateTimeField('data published')
 
     def __str__(self):
-        s = self.pub_date.strftime("%Y-%m-%d %H:%M:%S")
-        return 'The question is published on' + s + ' and the content is :' + self.question_text
+        # s = self.pub_date.strftime("%Y-%m-%d %H:%M:%S")
+        # return 'The question is published on' + s + ' and the content is :' + self.question_text
+        return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 class Choice(models.Model):
 
--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
+
 urlpatterns=[
-    path('', views.index, name='index'),
-    path('<int:question_id>/', views.detail, name='detail'),
-    path('<int:question_id>/results/', views.result, name='detail'),
+    # path('', views.index, name='index'),
+    # path('specifics/<int:question_id>/', views.detail, name='detail'),
+    # path('<int:question_id>/results/', views.result, name='result'),
+    # path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultView.as_view(), name='result'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
